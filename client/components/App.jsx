494ea41a0de47999f3b38536e38bec7c2c089af4
AnnaUlyanova <a.ulyanova89@gmail.com>
2017-05-31 11:18:43 +1200

    update react version, change createClass in App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import CreateReactClass from 'create-react-class'
 
-export default React.createClass({
+export default CreateReactClass({
   render() {
     return (
       <h1>Manage My Money</h1>
